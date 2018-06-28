5664802f10dd3638d63019a424e1eb6ca5e578f9
wangyuheng <wangyuheng@outlook.com>
2018-06-28 16:02:42 +0800

    init

---
+++ src/main/java/wang/crick/ddl2plantuml/dictionary/IndexTypeEnum.java
@@ -0,0 +1,13 @@
+package wang.crick.ddl2plantuml.dictionary;
+
+/**
+ * 索引类型
+ *
+ * @author wangyuheng
+ * @date 2018/6/28 13:29
+ */
+public enum IndexTypeEnum {
+    primary,
+    unique,
+    ordinary;
+}
